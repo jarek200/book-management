4e9f1cb084fabfbcd9b7010f677def324360a8f8
Jarek Wyprzal <jwyprzal@gmail.com>
2024-01-21 15:36:12 +0000

    set up actions and reducers

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,15 @@ import { StoreModule } from '@ngrx/store';
 import { BookReducer } from './books/book.reducer';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BookListComponent } from './book-list/book-list.component';
+import { AppState } from './app.state';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, BookListComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    StoreModule.forRoot({ book: BookReducer }),
+    StoreModule.forRoot<AppState>({ book: BookReducer }),
   ],
   providers: [],
   bootstrap: [AppComponent],
